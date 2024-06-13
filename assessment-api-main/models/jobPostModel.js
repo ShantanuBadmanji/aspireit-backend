1b98365360344f0b840a766f933727c949b4cb59
Rushabh <rushabhkhandhar38@.com>
2024-06-13 10:04:48 +0530

    added the jobPost schema

---
+++ assessment-api-main/models/jobPostModel.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+
+const jobPostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    company: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+        required: true,
+      },
+      remote: {
+        type: Boolean,
+        default: false,
+      },
+      status: {
+        type: String,
+        enum: ['Open', 'Closed'],
+        default: 'Open',
+      },
+      postedAt: {
+        type: Date,
+        default: Date.now,
+      },
+      applicantsCount: {
+        type: Number,
+        default: 0,
+      },
+      views: {
+        type: Number,
+        default: 0,
+      },
+      spent: {
+        type: Number,
+        default: 0,
+      },
+      dailyBudget: {
+        type: Number,
+        default: 0,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      screeningQuestions: {
+        type: String,
+        required: true,
+      },
+    });
+
+    const JobPost = mongoose.model('JobPost', jobPostSchema);
+    export default JobPost;
+    
