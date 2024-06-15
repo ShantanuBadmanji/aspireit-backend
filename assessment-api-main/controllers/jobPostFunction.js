8b5f50b004fd592233b9c9b0c905b092b7594d1c
Shantanu Badmanji <shantanubadmanji1912@gmail.com>
2024-06-15 17:36:19 +0530

    feat: Add CRUD functionality for job posts

--- assessment-api-main/controllers/jobPostFunction.js
+++ assessment-api-main/controllers/jobPostFunction.js
@@ -1,101 +1,133 @@
 import JobPost from "../models/jobPostModel.js";
 
+const getJobposts = async (req, res) => {
+  try {
+    const jobPosts = await JobPost.find();
+    res.json(jobPosts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-const getJobposts = async (req,res) =>{
-    try{
-        const jobPosts= await JobPost.find();
-        res.json(jobPosts)
-
+const getJobpostsById = async (req, res) => {
+  try {
+    const jobPost = await JobPost.findById(req.params.id);
+    if (jobPost) {
+      res.json(jobPost);
+    } else {
+      res.status(404).json({ message: "Job Post not found" });
     }
-    catch(error){
-
-        res.status(500).json({message: error.message});
-
-    }
-}
-
-const getJobpostsById = async (req,res) =>{
-    try{
-    
-        const jobPost = await JobPost.findById(req.params.id)
-        if(jobPost){
-            res.json(jobPost)
-        }
-        else
-        {
-            res.status(404).json({message: "Job Post not found"})
-        }
-    
-}
-    catch(error){
-        res.status(500).json({message: error.message});
-    }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const createJobPost = async (req, res) => {
-    const { title, company, location, remote, description, screeningQuestions, spent, dailyBudget } = req.body;
-  
-    try {
-      const jobPost = new JobPost({
-        title,
-        company,
-        location,
-        remote,
-        description,
-        screeningQuestions,
-        spent,
-        dailyBudget,
-      });
-  
-      const createdJobPost = await jobPost.save();
-      res.status(201).json(createdJobPost);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  };
+  const {
+    title,
+    company,
+    location,
+    remote,
+    description,
+    screeningQuestions,
+    spent,
+    dailyBudget,
+  } = req.body;
 
- const updateJobPost = async (req, res) => {
-    const { title, company, location, remote, description, screeningQuestions, spent, dailyBudget } = req.body;
-    
-    try{
-        const jobPost = await JobPost.findById(req.params.id);
-        if(jobPost){
-            jobPost.title = title;
-            jobPost.company = company;
-            jobPost.location = location;
-            jobPost.remote = remote;
-            jobPost.description = description;
-            jobPost.screeningQuestions = screeningQuestions;
-            jobPost.spent = spent;
-            jobPost.dailyBudget = dailyBudget;
-            const updatedJobPost = await jobPost.save();
-            res.status(201).json(updatedJobPost);
-        
-    }
-    else{
-        res.status(404).json({message: "Job Post not found"})
-    }
-}
-catch(error){
-    res.status(400).json({message: error.message});
-}
- }
+  try {
+    const jobPost = new JobPost({
+      title,
+      company,
+      location,
+      remote,
+      description,
+      screeningQuestions,
+      spent,
+      dailyBudget,
+    });
 
- const deleteJobPost = async (req, res) => {
-    try {
-      const jobPost = await JobPost.findById(req.params.id);
-  
-      if (!jobPost) {
-        return res.status(404).json({ message: 'Job post not found' });
-      }
-  
-      await jobPost.deleteOne(); 
-  
-      res.json({ message: 'Job post removed' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
-    }
-  };
+    const createdJobPost = await jobPost.save();
+    res.status(201).json(createdJobPost);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
-export {getJobposts,getJobpostsById,createJobPost,updateJobPost,deleteJobPost};
+const updateJobPost = async (req, res) => {
+  const {
+    title,
+    company,
+    location,
+    remote,
+    description,
+    screeningQuestions,
+    spent,
+    dailyBudget,
+  } = req.body;
+
+  try {
+    const jobPost = await JobPost.findById(req.params.id);
+    if (jobPost) {
+      jobPost.title = title;
+      jobPost.company = company;
+      jobPost.location = location;
+      jobPost.remote = remote;
+      jobPost.description = description;
+      jobPost.screeningQuestions = screeningQuestions;
+      jobPost.spent = spent;
+      jobPost.dailyBudget = dailyBudget;
+      const updatedJobPost = await jobPost.save();
+      res.status(201).json(updatedJobPost);
+    } else {
+      res.status(404).json({ message: "Job Post not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const deleteJobPost = async (req, res) => {
+  try {
+    const jobPost = await JobPost.findById(req.params.id);
+
+    if (!jobPost) {
+      return res.status(404).json({ message: "Job post not found" });
+    }
+
+    await jobPost.deleteOne();
+
+    res.json({ message: "Job post removed" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+const applyJobPost = async (req, res) => {
+  const { jobApplicantId } = req.body;
+  try {
+    const updatedJobPost = await JobPost.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: { applicants: jobApplicantId },
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (updatedJobPost) {
+      res.json(updatedJobPost);
+    } else {
+      res.status(404).json({ message: "Job Post not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export {
+  getJobposts,
+  getJobpostsById,
+  createJobPost,
+  updateJobPost,
+  deleteJobPost,
+  applyJobPost,
+};
--- assessment-api-main/routes/jobPostRoutes.js
+++ assessment-api-main/routes/jobPostRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import {getJobposts,getJobpostsById,createJobPost,updateJobPost,deleteJobPost} from '../controllers/jobPostFunction.js';
+import {getJobposts,getJobpostsById,createJobPost,updateJobPost,deleteJobPost, applyJobPost} from '../controllers/jobPostFunction.js';
 
 const router = express.Router();
 
@@ -12,5 +12,8 @@ router.route('/:id')
   .get(getJobpostsById)
   .put(updateJobPost)
   .delete(deleteJobPost);
+
+router.route('/:id/apply')
+  .post(applyJobPost);
 
 export default router;
